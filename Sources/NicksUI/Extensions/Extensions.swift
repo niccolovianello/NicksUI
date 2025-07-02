191787ca92950744cbd0a9db9294c66b8310ad18
Niccolò Vianello <n.vianello@reply.it>
2025-07-02 23:39:01 +0200

    makes extension public

--- Sources/NicksUI/Extensions/Extensions.swift
+++ Sources/NicksUI/Extensions/Extensions.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 
-extension View {
+public extension View {
     func borderedTextField(textPadding: CGFloat = 8, cornerRadius: CGFloat = 8, strokeColor: Color = .black, lineWidth: CGFloat = 1) -> some View {
         self
             .padding(textPadding)
