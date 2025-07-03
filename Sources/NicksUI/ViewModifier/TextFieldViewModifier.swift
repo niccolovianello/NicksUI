b4b6008182112f88c41473f49e6c5bdbcfd7880f
Niccolò Vianello <n.vianello@reply.it>
2025-07-03 17:06:19 +0200

    separated vertical and horizontal padding in textFieldViewModifier

--- Sources/NicksUI/ViewModifier/TextFieldViewModifier.swift
+++ Sources/NicksUI/ViewModifier/TextFieldViewModifier.swift
@@ -10,14 +10,16 @@ import SwiftUI
 
 struct TextFieldViewModifier: ViewModifier {
     
-    var textPadding: CGFloat
+    var horizontalTextPadding: CGFloat
+    var verticalTextPadding: CGFloat
     var cornerRadius: CGFloat
     var strokeColor: Color
     var lineWidth: CGFloat
     
     func body(content: Content) -> some View {
         content
-            .padding(textPadding)
+            .padding(.vertical, verticalTextPadding)
+            .padding(.horizontal, horizontalTextPadding)
             .overlay(
                 RoundedRectangle(cornerRadius: cornerRadius)
                     .stroke(strokeColor, lineWidth: lineWidth)
@@ -26,7 +28,7 @@ struct TextFieldViewModifier: ViewModifier {
 }
 
 public extension View {
-    func borderedTextField(textPadding: CGFloat = 8, cornerRadius: CGFloat = 8, strokeColor: Color = .black, lineWidth: CGFloat = 1) -> some View {
-        modifier(TextFieldViewModifier(textPadding: textPadding, cornerRadius: cornerRadius, strokeColor: strokeColor, lineWidth: lineWidth))
+    func borderedTextField(horizontalTextPadding: CGFloat = 8, verticalTextPadding: CGFloat = 8, cornerRadius: CGFloat = 8, strokeColor: Color = .black, lineWidth: CGFloat = 1) -> some View {
+        modifier(TextFieldViewModifier(horizontalTextPadding: horizontalTextPadding, verticalTextPadding: verticalTextPadding, cornerRadius: cornerRadius, strokeColor: strokeColor, lineWidth: lineWidth))
     }
 }
